a2e300ba4eb850a99197e9f7276c5803885d7baf
Egor Bychin <e.siga@proton.me>
2024-05-12 16:37:36 +0400

    fix ignore

--- src/siga/util/toy.hpp
+++ src/siga/util/toy.hpp
@@ -49,8 +49,7 @@ public:
 class ignore_t
 {
 public:
-    constexpr ignore_t &operator=(auto &&) noexcept { return *this; }
-    constexpr void operator()(auto &&...) noexcept {}
+    constexpr void operator()(auto &&...) const noexcept {}
 };
 
 constexpr inline ignore_t ignore;
