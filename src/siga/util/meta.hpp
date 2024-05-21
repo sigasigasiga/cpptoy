25b3e0043888a008457d2101ab4075050f50b09f
Egor Bychin <e.siga@proton.me>
2024-05-21 13:33:56 +0400

    add trait + small improvements in `app.cpp`

--- src/siga/util/meta.hpp
+++ src/siga/util/meta.hpp
@@ -6,4 +6,27 @@ namespace siga::util {
 template<typename T, template<typename...> typename Trait>
 concept conceptify = Trait<T>::value;
 
+// ------------------------------------------------------------------------------------------------
+
+template<typename T, template<typename...> typename Trait, template<typename...> typename... Rest>
+class combine
+{
+public:
+    using type = combine<typename Trait<T>::type, Rest...>::type;
+};
+
+template<typename T, template<typename...> typename Trait>
+class combine<T, Trait>
+{
+public:
+    using type = Trait<T>::type;
+};
+
+template<typename T, template<typename...> typename... Traits>
+using combine_t = combine<T, Traits...>::type;
+
+#if 0
+static_assert(std::same_as<combine_t<const int &, std::remove_reference, std::remove_cv>, int>);
+#endif
+
 } // namespace siga::util
