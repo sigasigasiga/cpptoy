1e27eb0123a56fb8763740b1641ef2a934292a2d
Egor Bychin <e.siga@proton.me>
2024-05-29 16:04:23 +0400

    print all the addresses in `printing_constructor`

--- src/siga/util/toy.hpp
+++ src/siga/util/toy.hpp
@@ -20,22 +20,46 @@ class printing_constructor
 public:
     printing_constructor() { std::cout << "printing_constructor(): " << this << std::endl; }
 
-    printing_constructor(const printing_constructor &) {
-        std::cout << "printing_constructor(const printing_constructor &)" << std::endl;
+    printing_constructor(const printing_constructor &rhs) {
+        // clang-format off
+        std::cout
+            << "printing_constructor(const printing_constructor &); "
+            << "lhs: " << this << ", "
+            << "rhs: " << &rhs
+            << std::endl;
+        // clang-format on
     }
 
-    printing_constructor(printing_constructor &&) {
-        std::cout << "printing_constructor(printing_constructor &&)" << std::endl;
+    printing_constructor(printing_constructor &&rhs) {
+        // clang-format off
+        std::cout
+            << "printing_constructor(printing_constructor &&); "
+            << "lhs: " << this << ", "
+            << "rhs: " << &rhs
+            << std::endl;
+        // clang-format on
     }
 
-    printing_constructor &operator=(const printing_constructor &) {
-        std::cout << "printing_constructor &operator=(const printing_constructor &)" << std::endl;
+    printing_constructor &operator=(const printing_constructor &rhs) {
+        // clang-format off
+        std::cout
+            << "printing_constructor &operator=(const printing_constructor &); " 
+            << "lhs: " << this << ", "
+            << "rhs: " << &rhs
+            << std::endl;
+        // clang-format on
 
         return *this;
     }
 
-    printing_constructor &operator=(printing_constructor &&) {
-        std::cout << "printing_constructor &operator=(printing_constructor &&)" << std::endl;
+    printing_constructor &operator=(printing_constructor &&rhs) {
+        // clang-format off
+        std::cout
+            << "printing_constructor &operator=(printing_constructor &&); " 
+            << "lhs: " << this << ", "
+            << "rhs: " << &rhs
+            << std::endl;
+        // clang-format on
 
         return *this;
     }
