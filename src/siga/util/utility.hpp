0f043bfd0af46a7335c6f520e5f3729580022236
Egor Bychin <e.siga@proton.me>
2024-05-30 12:21:34 +0400

    make `cut_rvalue_ref` a niebloid

--- src/siga/util/utility.hpp
+++ src/siga/util/utility.hpp
@@ -4,15 +4,21 @@ namespace siga::util {
 
 // `util/utility.hpp` sounds funny, but I was trying to somehow resemble the `<utility>` STL header
 
-template<typename T>
-[[nodiscard]] constexpr decltype(auto) cut_rvalue_ref(T &&v) noexcept
+class [[nodiscard]] cut_rvalue_ref_t
 {
-    // cut_rvalue_ref(0) -> cut_rvalue_ref<int>(int &&);
-    // cut_rvalue_ref(x) -> cut_rvalue_ref<int &>(int &);
-    //
-    // so if the passed value is lvalue, we return the lvalue,
-    // and if the passed value is rvalue, we return the value
-    return static_cast<T>(std::forward<T>(v));
-}
+public:
+    template<typename T>
+    [[nodiscard]] static constexpr decltype(auto) operator()(T &&v) noexcept
+    {
+        // cut_rvalue_ref(0) -> cut_rvalue_ref<int>(int &&);
+        // cut_rvalue_ref(x) -> cut_rvalue_ref<int &>(int &);
+        //
+        // so if the passed value is lvalue, we return the lvalue,
+        // and if the passed value is rvalue, we return the value
+        return static_cast<T>(std::forward<T>(v));
+    }
+};
+
+inline constexpr cut_rvalue_ref_t cut_rvalue_ref;
 
 } // namespace siga::util
