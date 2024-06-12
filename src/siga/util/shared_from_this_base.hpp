83ff7049ad129849221394e364ab8a637bb5d1a6
Egor Bychin <e.siga@proton.me>
2024-06-13 00:34:42 +0400

    delegate sftb to esft

--- src/siga/util/shared_from_this_base.hpp
+++ src/siga/util/shared_from_this_base.hpp
@@ -7,16 +7,17 @@ namespace siga::util {
 //
 // this approach has some problems:
 // 1. boilerplate
-// 2. it doesn't work great if we want to derive from `A`
-// 3. it makes `std::make_shared` usage tricky
-// 4. if we want to inherit from multiple classes that are derived from esft,
-//    everything becomes a huge mess, and there are no ways to fix that
-//    because it is impossible to inherit from it virtually
+// 2. if we want to derive `B` from `A`, `A::make` will still be available by default
+// 3. it makes `std::make_shared` usage tricky because of `A`'s private constructor
+//
+// furthermore, multiple inheritance from esft-enabled classes is also a pain:
+// 1. the class will have excess `weak_ptr`s in it
+// 2. `.shared_from_this()` won't be callable directly
 //
 // `shared_from_this_base` solves these problems:
 // 1. the only thing that sftb-derived classes need to do
-//    is to put `access_tag` as the first constructor argument and init the base class with it
-// 2. classes that are derived from `A` also need to put `access_tag` as the first constructor arg,
+//    is to put `sftb_tag` as the first constructor argument and init the base class with it
+// 2. classes that are derived from `A` also need to put `sftb_tag` as the first constructor arg,
 //    and then they should forward it to the derived class
 // 3. `util::make_shared` becomes the only way to construct it and it is optimal
 // 4. virtual inheritance from sftb is easy
@@ -25,23 +26,23 @@ class shared_from_this_base;
 template<typename T>
 concept sftb_makeable = std::convertible_to<T *, const volatile shared_from_this_base *>;
 
-class shared_from_this_base
+class shared_from_this_base : public std::enable_shared_from_this<shared_from_this_base>
 {
 public:
     template<sftb_makeable T>
-    class make_shared_impl;
+    class make_shared_with_sftb_tag;
 
-    class access_tag
+    class sftb_tag
     {
     private:
-        constexpr explicit access_tag() = default;
+        constexpr explicit sftb_tag() = default;
 
         template<sftb_makeable T>
-        friend class make_shared_impl;
+        friend class make_shared_with_sftb_tag;
     };
 
 protected:
-    explicit shared_from_this_base(access_tag) noexcept {}
+    constexpr explicit shared_from_this_base(sftb_tag) noexcept {}
 
 public:
     [[nodiscard]] auto shared_from_this(this auto &&self) { return self.make_sft(); }
@@ -49,9 +50,15 @@ public:
 
 private:
     template<typename Self>
+    auto raw_sft(this Self &self)
+    {
+        return self.enable_shared_from_this::shared_from_this();
+    }
+
+    template<typename Self>
     std::shared_ptr<Self> make_sft(this Self &self)
     {
-        auto ret = std::static_pointer_cast<Self>(self.weak_this_.lock());
+        auto ret = std::static_pointer_cast<Self>(self.raw_sft());
         assert(ret && "how have you constructed such object?");
         return ret;
     }
@@ -61,32 +68,26 @@ private:
     {
         return self.make_sft();
     }
-
-private:
-    mutable std::weak_ptr<void> weak_this_;
 };
 
 template<sftb_makeable T>
-class shared_from_this_base::make_shared_impl
+class shared_from_this_base::make_shared_with_sftb_tag
 {
 private:
-    using access_tag = shared_from_this_base::access_tag;
+    using sftb_tag = shared_from_this_base::sftb_tag;
 
 public:
     template<typename... Args>
-    requires std::constructible_from<T, access_tag, Args &&...>
-    [[nodiscard]] static std::shared_ptr<T> operator()(Args &&...args)
+    requires std::constructible_from<T, sftb_tag, Args &&...>
+    [[nodiscard]] static auto operator()(Args &&...args)
     {
-        auto s = std::make_shared<T>(access_tag{}, std::forward<Args>(args)...);
-        s->weak_this_ = s;
-        return s;
+        return std::make_shared<T>(sftb_tag{}, std::forward<Args>(args)...);
     }
 };
 
 template<typename T>
 class universal_make_shared
 {
-public:
     template<typename... Args>
     requires std::constructible_from<T, Args &&...>
     [[nodiscard]] static auto operator()(Args &&...args)
@@ -96,7 +97,7 @@ public:
 };
 
 template<sftb_makeable T>
-class universal_make_shared<T> : public shared_from_this_base::make_shared_impl<T>
+class universal_make_shared<T> : public shared_from_this_base::make_shared_with_sftb_tag<T>
 {};
 
 template<typename T>
