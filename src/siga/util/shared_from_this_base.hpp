cf28f18c4ce54c284b9bc9d9fc4c0650c9fc35e3
Egor Bychin <e.siga@proton.me>
2024-06-13 13:27:07 +0400

    `esft_common_base`

--- src/siga/util/shared_from_this_base.hpp
+++ src/siga/util/shared_from_this_base.hpp
@@ -2,6 +2,45 @@
 
 namespace siga::util {
 
+// multiple inheritance from esft-enabled classes is a pain:
+// 1. the class will have excess `weak_ptr`s in it
+// 2. `.shared_from_this()` won't be callable directly
+//
+// `esft_common_base` solves these problems by letting its users inherit virtually and
+// providing `shared_from_this` and `weak_from_this` that are implemented with deducing this
+class esft_common_base : public std::enable_shared_from_this<esft_common_base>
+{
+public:
+    [[nodiscard]] auto shared_from_this(this auto &&self) { return self.make_sft(); }
+    [[nodiscard]] auto weak_from_this(this auto &&self) noexcept { return self.make_wft(); }
+
+private:
+    template<typename Self>
+    auto raw_sft(this Self &self)
+    {
+        return self.enable_shared_from_this::shared_from_this();
+    }
+
+    template<typename Self>
+    std::shared_ptr<Self> make_sft(this Self &self)
+    {
+        return std::static_pointer_cast<Self>(self.raw_sft());
+    }
+
+    template<typename Self>
+    auto raw_wft(this Self &self) noexcept
+    {
+        return self.enable_shared_from_this::weak_from_this();
+    }
+
+    template<typename Self>
+    std::weak_ptr<Self> make_wft(this Self &self) noexcept
+    {
+        // `weak_from_this` should never throw, thus it cannot be delegated to `make_sft`
+        return std::static_pointer_cast<Self>(self.raw_wft().lock());
+    }
+};
+
 // `std::enable_shared_from_this` requires its derived classes (let's call one `A`)
 // to have some sort of static method that would construct an instance of the class.
 //
@@ -10,23 +49,20 @@ namespace siga::util {
 // 2. if we want to derive `B` from `A`, `A::make` will still be available by default
 // 3. it makes `std::make_shared` usage tricky because of `A`'s private constructor
 //
-// furthermore, multiple inheritance from esft-enabled classes is also a pain:
-// 1. the class will have excess `weak_ptr`s in it
-// 2. `.shared_from_this()` won't be callable directly
-//
 // `shared_from_this_base` solves these problems:
 // 1. the only thing that sftb-derived classes need to do
 //    is to put `sftb_tag` as the first constructor argument and init the base class with it
 // 2. classes that are derived from `A` also need to put `sftb_tag` as the first constructor arg,
 //    and then they should forward it to the derived class
 // 3. `util::make_shared` becomes the only way to construct it and it is optimal
-// 4. virtual inheritance from sftb is easy
+//
+// it is also derived from `esft_common_base` to solve the problem with multiple inheritance
 class shared_from_this_base;
 
 template<typename T>
 concept sftb_makeable = std::convertible_to<T *, const volatile shared_from_this_base *>;
 
-class shared_from_this_base : public std::enable_shared_from_this<shared_from_this_base>
+class shared_from_this_base : public esft_common_base
 {
 public:
     template<sftb_makeable T>
@@ -43,31 +79,6 @@ public:
 
 protected:
     constexpr explicit shared_from_this_base(sftb_tag) noexcept {}
-
-public:
-    [[nodiscard]] auto shared_from_this(this auto &&self) { return self.make_sft(); }
-    [[nodiscard]] auto weak_from_this(this auto &&self) { return self.make_wft(); }
-
-private:
-    template<typename Self>
-    auto raw_sft(this Self &self)
-    {
-        return self.enable_shared_from_this::shared_from_this();
-    }
-
-    template<typename Self>
-    std::shared_ptr<Self> make_sft(this Self &self)
-    {
-        auto ret = std::static_pointer_cast<Self>(self.raw_sft());
-        assert(ret && "how have you constructed such object?");
-        return ret;
-    }
-
-    template<typename Self>
-    std::weak_ptr<Self> make_wft(this Self &self)
-    {
-        return self.make_sft();
-    }
 };
 
 template<sftb_makeable T>
