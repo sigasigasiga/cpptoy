365fac7baac13f723538239ffcfca58165485844
Egor Bychin <e.siga@proton.me>
2024-06-24 17:03:14 +0400

    add `string_literal`

--- src/siga/util/meta.hpp
+++ src/siga/util/meta.hpp
@@ -42,6 +42,35 @@ class [[nodiscard]] type_list
 
 // ------------------------------------------------------------------------------------------------
 
+template<std::size_t N>
+struct [[nodiscard]] string_literal
+{
+public:
+    static_assert(N > 0);
+    static constexpr std::size_t size = N - 1;
+
+public:
+    constexpr string_literal() = default;
+
+    constexpr string_literal(const char (&str)[N])
+    {
+        if(str[N - 1] == '\0') {
+            std::ranges::copy(str, std::ranges::begin(data));
+        } else {
+            throw std::logic_error{"the string must be null-terminated"};
+        }
+    }
+
+public:
+    [[nodiscard]] constexpr std::string_view as_view() const noexcept { return data; }
+    [[nodiscard]] constexpr operator std::string_view() const noexcept { return as_view(); }
+
+public:
+    char data[N] = {};
+};
+
+// ------------------------------------------------------------------------------------------------
+
 template<typename From, typename To>
 class copy_const
 {
