4859413bcfae74be47c4d1e45f883c4629497e0c
Egor Bychin <e.siga@proton.me>
2024-06-24 17:02:47 +0400

    mark `fold_invoke` as `noexcept`

--- src/siga/util/functional.hpp
+++ src/siga/util/functional.hpp
@@ -245,12 +245,54 @@ private:
 
 // ------------------------------------------------------------------------------------------------
 
+template<typename FsTuple, typename... InnerArgs>
+class is_nothrow_fold_invocable
+{
+private:
+    template<typename IndexSequence, typename... Args>
+    class impl;
+
+    template<std::size_t I, typename... Args>
+    class impl<std::index_sequence<I>, Args...>
+    {
+    private:
+        using current_fn_t = tuple_get_type_t<FsTuple, I>;
+
+    public:
+        static constexpr bool value = std::is_nothrow_invocable_v<current_fn_t, Args...>;
+    };
+
+    template<std::size_t I, std::size_t... Is, typename... Args>
+    class impl<std::index_sequence<I, Is...>, Args...>
+    {
+    private:
+        using current_fn_t = tuple_get_type_t<FsTuple, I>;
+        using current_ret_t = std::invoke_result_t<current_fn_t, Args...>;
+
+        static constexpr bool current = std::is_nothrow_invocable_v<current_fn_t, Args...>;
+
+    public:
+        static constexpr bool value =
+            current && impl<std::index_sequence<Is...>, current_ret_t>::value;
+    };
+
+private:
+    static constexpr std::size_t tuple_size = std::tuple_size_v<std::decay_t<FsTuple>>;
+
+public:
+    static constexpr bool value = impl<std::make_index_sequence<tuple_size>, InnerArgs...>::value;
+};
+
+template<typename FsTuple, typename... Args>
+inline constexpr bool is_nothrow_fold_invocable_v =
+    is_nothrow_fold_invocable<FsTuple, Args...>::value;
+
 class [[nodiscard]] fold_invoke_t
 {
 public:
-    // TODO: noexcept
     template<typename FuncTuple, typename... Args>
     static constexpr decltype(auto) operator()(FuncTuple &&funcs, Args &&...args)
+        noexcept(is_nothrow_fold_invocable_v<FuncTuple &&, Args &&...>)
     {
         return impl(
             std::make_index_sequence<std::tuple_size_v<std::decay_t<FuncTuple>>>{},
