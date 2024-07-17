7e34ff8172231909f41d6cca92bfc5163964f77e
Egor Bychin <e.siga@proton.me>
2024-07-17 23:36:20 +0400

    make `SIGA_UTIL_LIFT_MEMBER` satisfy `INVOKE`

--- src/siga/util/functional.hpp
+++ src/siga/util/functional.hpp
@@ -534,44 +534,60 @@ class std::is_bind_expression<siga::util::make_bind_expression<F>> : public std:
 // Known limitations:
 // 1. Resulting lambda never produces an overload set.
 //    That is, if passed to `util::overload`, the result may be surprising
-// 2. If `MEMBER` is a niebloid, it'd be called,
-//    despite the fact that `std::invoke` would return a reference to it.
+// 2. If `MEMBER` is a niebloid, it'd be called, despite the fact
+//    that `std::invoke` would return a reference to it.
 //    While this is probably fixable, I'm not sure if it's worth the effort
 // 
 // TODO: should we also specify a class name? If so, don't forget that types must be _compatible_
-#define SIGA_UTIL_LIFT_MEMBER(MEMBER)                                                              \
-    []<typename T, typename... Args>                                                               \
-        (T &&value, Args &&...args)                                                                \
-        constexpr                                                                                  \
-        static                                                                                     \
-        noexcept(                                                                                  \
-            !requires { std::forward<T>(value).MEMBER(std::forward<Args>(args)...); } ||           \
-            requires {                                                                             \
-                requires(noexcept(std::forward<T>(value).MEMBER(std::forward<Args>(args)...)));    \
-            }                                                                                      \
-        )                                                                                          \
-        -> decltype(auto)                                                                          \
-        requires requires { std::forward<T>(value).MEMBER(std::forward<Args>(args)...); } ||       \
-                 requires { std::forward<T>(value).MEMBER; }                                       \
-    {                                                                                              \
-        constexpr bool is_method = requires {                                                      \
-            std::forward<T>(value).MEMBER(std::forward<Args>(args)...);                            \
-        };                                                                                         \
-                                                                                                   \
-        constexpr bool is_field = requires {                                                       \
-            std::forward<T>(value).MEMBER;                                                         \
-        };                                                                                         \
-                                                                                                   \
-        if constexpr(is_method) {                                                                  \
-            return std::forward<T>(value).MEMBER(std::forward<Args>(args)...);                     \
-        } else if constexpr(is_field) {                                                            \
-            /* Parenthesis around needed for `decltype(auto)` */                                   \
-            return (std::forward<T>(value).MEMBER);                                                \
-        } else {                                                                                   \
-            static_assert(                                                                         \
-                false,                                                                             \
-                "Must be unreachable (`" #MEMBER "`)"                                              \
-            );                                                                                     \
-        }                                                                                          \
+#define SIGA_UTIL_LIFT_MEMBER(MEMBER) \
+    []< \
+        typename T, \
+        typename... Args, \
+        bool IsRefWrapper = ::std::same_as< \
+            decltype(::std::ref(::std::declval<T &>())), \
+            ::std::decay_t<T> \
+        > \
+    > \
+        (T &&value, Args &&...args) \
+        constexpr \
+        static \
+        noexcept( \
+            [] { \
+                if constexpr(requires { ::std::forward<T>(value).MEMBER(::std::forward<Args>(args)...); }) { \
+                    return noexcept(::std::forward<T>(value).MEMBER(::std::forward<Args>(args)...)); \
+                } else if constexpr(IsRefWrapper && requires { value.get().MEMBER(::std::forward<Args>(args)...); }) { \
+                    return noexcept(value.get().MEMBER(::std::forward<Args>(args)...)); \
+                } else if constexpr(requires { (*::std::forward<T>(value)).MEMBER(::std::forward<Args>(args)...); }) { \
+                    return noexcept((*::std::forward<T>(value)).MEMBER(::std::forward<Args>(args)...)); \
+                } else if constexpr(requires { (*::std::forward<T>(value)).MEMBER; }) { \
+                    return noexcept(*::std::forward<T>(value)); \
+                } else { \
+                    return true; \
+                } \
+            }() \
+        ) \
+        -> decltype(auto) \
+        requires \
+            requires { ::std::forward<T>(value).MEMBER(::std::forward<Args>(args)...); } || \
+            (IsRefWrapper && requires { value.get().MEMBER(::std::forward<Args>(args)...); }) || \
+            requires { (*::std::forward<T>(value)).MEMBER(::std::forward<Args>(args)...); } || \
+            requires { ::std::forward<T>(value).MEMBER; } || \
+            (IsRefWrapper && requires { value.get().MEMBER; }) || \
+            requires { (*::std::forward<T>(value)).MEMBER; } \
+    { \
+        if constexpr(requires { ::std::forward<T>(value).MEMBER(::std::forward<Args>(args)...); }) { \
+            return ::std::forward<T>(value).MEMBER(::std::forward<Args>(args)...); \
+        } else if constexpr(IsRefWrapper && requires { value.get().MEMBER(::std::forward<Args>(args)...); }) { \
+            return value.get().MEMBER(::std::forward<Args>(args)...); \
+        } else if constexpr(requires { (*::std::forward<T>(value)).MEMBER(::std::forward<Args>(args)...); }) { \
+            return (*::std::forward<T>(value)).MEMBER(::std::forward<Args>(args)...); \
+        } else if constexpr(requires { ::std::forward<T>(value).MEMBER; }) { \
+            /* Parenthesis around needed for `decltype(auto)` */ \
+            return (::std::forward<T>(value).MEMBER); \
+        } else if constexpr(IsRefWrapper && requires { value.get().MEMBER; }) { \
+            return (value.get().MEMBER); \
+        } else if constexpr(requires { (*::std::forward<T>(value)).MEMBER; }) { \
+            return ((*::std::forward<T>(value)).MEMBER); \
+        } \
     }
 // clang-format on
