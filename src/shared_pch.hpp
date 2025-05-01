8fec0458155c3e0e71d2375eee84d3b6a75aad77
Egor Bychin <e.siga@proton.me>
2025-05-01 22:48:08 +0400

    fix build

--- src/shared_pch.hpp
+++ src/shared_pch.hpp
@@ -43,12 +43,3 @@
 // thirdparty
 #include <fmt/core.h>
 #include <range/v3/all.hpp>
-
-#include <siga/algo.hpp>
-#include <siga/compat.hpp>
-#include <siga/fn.hpp>
-#include <siga/iter.hpp>
-#include <siga/meta.hpp>
-#include <siga/ranges.hpp>
-#include <siga/toy.hpp>
-#include <siga/util.hpp>
